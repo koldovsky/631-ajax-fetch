1ae6bf40c16c518a15c3996cc585afec6d50aa9d
koldovsky <koldovsky@koldovsky.com>
2021-09-29 20:13:28 +0300

    Ajax/Fetch

--- js/product-list.js
+++ js/product-list.js
@@ -1,36 +1,29 @@
-const products = [
-    {
-        id: 1,
-        name: "Baby Yoda",
-        img: 'img/baby-yoda.svg',
-        price: 10.99,
-        description: 'Some <b>quick</b> example text to build on the card title and make up the bulk of the cards content.'
-    },
-    {
-        id: 2,
-        name: "Banana",
-        img: 'img/banana.svg',
-        price: 8.99,
-        description: 'Some <b>quick</b> example text to build on the card title and make up the bulk of the cards content.'
-    },
-    {
-        id: 3,
-        name: "Girl",
-        img: 'img/girl.svg',
-        price: 9.99,
-        description: 'Some <b>quick</b> example text to build on the card title and make up the bulk of the cards content.'
-    },
-    {
-        id: 4,
-        name: "Viking",
-        img: 'img/viking.svg',
-        price: 7.99,
-        description: 'Some <b>quick</b> example text to build on the card title and make up the bulk of the cards content.'
-    }
-]
+async function getJson(url) {
+    const response = await fetch(url);
+    return await response.json();
+}
+
+// function getJsonPromise(url, callback) {
+//     fetch(url)
+//         .then( responce => responce.json() )
+//         .then( data => callback(data));
+// }
+
+// function getJsonWithAjax(url, callback) {
+//     const xhr = new XMLHttpRequest();
+//     xhr.onreadystatechange = function() {
+//         if (xhr.readyState === 4 && xhr.status === 200) {
+//             const data = JSON.parse(xhr.responseText);
+//             callback(data);
+//         }
+//     }
+//     xhr.open('GET', url, true);
+//     xhr.send();
+// }
 
 function renderProducts(products) {
     const productsContainer = document.querySelector('.products');
+    productsContainer.innerHTML = '';
     for (const product of products) {
         productsContainer.innerHTML += 
             `<article class="card col-12 col-sm-6 col-md-4 col-lg-3">
@@ -39,9 +32,34 @@ function renderProducts(products) {
                 <h5 class="card-title">${product.name}</h5>
                 <p class="card-text">${product.description}</p>
                 <a href="#" class="btn btn-info">Info</a>
-                <a href="#" class="btn btn-primary">${product.price} - Buy</a>
+                <a href="#" class="btn btn-primary">${product.convertedPrice || product.price} - Buy</a>
                 </div>
             </article>`;
     }   
 }
-renderProducts(products);
+
+(async function () {
+    const products = await getJson('products.json'); 
+    renderProducts(products);
+})();
+
+// getJsonPromise('products.json', renderProducts);
+// getJsonWithAjax('products.json', renderProducts);
+
+async function convertCurrency() {
+    const convertTo = document.querySelector('.currency').value;
+    const currencies = await getJson('https://api.exchangerate-api.com/v4/latest/USD');
+    const rate = currencies.rates[convertTo];
+    const products = await getJson('products.json'); 
+    for (const product of products) {
+        product.convertedPrice = (product.price * rate).toFixed(2);
+    }
+    renderProducts(products);
+}
+
+
+
+document
+    .querySelector('.convert')
+    .addEventListener('click', convertCurrency);
+
